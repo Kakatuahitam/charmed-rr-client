00f090990c061c95dc55b2d6fe9dd9ad101641e0
Yuda Kurnia Nurul Fikri <yudakurnia.nurulfikri@gmail.com>
2025-07-09 19:32:58 +0700

    checkpoint

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,9 +1,9 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Welcome } from "../components/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "CharmedScout" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
